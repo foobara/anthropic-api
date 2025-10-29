273ea5bfd2d67661e36d33c34ff1f84fbf82320a
Miles Georgi <azimux@gmail.com>
2025-10-28 17:59:58 -0700

    Bump default since the old one seems to have been removed

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,8 +1,18 @@
 require "vcr"
 
 VCR.configure do |config|
+  config.filter_sensitive_data("<ANTHROPIC_API_KEY>") { ENV.fetch("ANTHROPIC_API_KEY", nil) }
+
+  config.before_record do |interaction|
+    if interaction.request.headers["Cookie"]
+      interaction.request.headers["Cookie"] = ["<SCRUBBED>"]
+    end
+    if interaction.response.headers["Set-Cookie"]
+      interaction.response.headers["Set-Cookie"] = ["<SCRUBBED>"]
+    end
+  end
+
   config.cassette_library_dir = "spec/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("<ANTHROPIC_API_KEY>") { ENV.fetch("ANTHROPIC_API_KEY", nil) }
 end
